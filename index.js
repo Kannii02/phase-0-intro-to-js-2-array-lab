ea9001d4fee7b1f8d69f62cb78c577e7f6762d09
kannikarsriburin <i_m_juy-juys@hotmail.co.th>
2025-01-04 15:06:09 -1000

    Completed Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution here!
+["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name); // Appends to the end of the array
+  }
+  function appendCat(name) {
+    return [...cats, name]; // Returns a new array
+  }
+  function destructivelyRemoveLastCat() {
+    cats.pop(); // Removes the last element
+  }
+  function removeLastCat() {
+    return cats.slice(0, -1); // Returns a new array without the last element
+  }
+  
